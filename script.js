724bf18ecf695144422600ff1bb644bd85d33060
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-16 00:33:58 +0000

    fixed all errors in JAVA

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//connect "Guess" button to "random" function
+//  connect "Guess" button to "random" function
 document.getElementById('button').addEventListener('click', comparison)
 let userInput = 0
 
-function comparison() {
-  //get user input and convert to an integer
+function comparison () {
+  //  get user input and convert to an integer
   userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
 
-  //check if userInput is less than 0
+  //  check if userInput is less than 0
   if (userInput < 0) {
-    document.getElementById('answer').innerHTML = "Negative"
+    document.getElementById('answer').innerHTML = 'Negative'
   } else {
-    document.getElementById('answer').innerHTML = "Positive"
-  }
+    document.getElementById('answer').innerHTML = 'Positive'
+}
 
 }
